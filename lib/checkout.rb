0e7a8cb6bcaee2511e26995eb41ecc6a9fc99c38
Mrkaykay <90140075+Mrkaykay@users.noreply.github.com>
2022-05-26 10:47:47 +0100

    checkout scans items and returns scannned item

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,13 +1,20 @@
 class Checkout
     PROMO =  {over60: ".10", two_or_more: "-8."}
 
+
     def initialize(promotional_rules = PROMO)
+        
     end
 
     def scan(item)
+        @item = item
     end
 
     def total
+    end
+
+    def item
+        @item
     end
 
 
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -11,5 +11,19 @@ describe Checkout do
     it { is_expected.to respond_to(:scan).with(1).argument }
 
     it { is_expected.to respond_to(:total) }
+
+    it { is_expected.to respond_to(:item)}
+
+    it 'scans items' do
+        item = {"001": "9.25"}
+        expect(subject.scan(item)).to eq item
+    end
+
+    it "returns scanned items" do
+        item = {"001": "9.25"}
+        subject.scan(item)
+        expect(subject.item).to eq item
+    end
+
 end
 
