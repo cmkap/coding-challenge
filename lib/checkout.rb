bd530fbd1f97df26fdedfc59ccff40badf013be6
Mrkaykay <90140075+Mrkaykay@users.noreply.github.com>
2022-05-26 16:42:00 +0100

    Refactored test context

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,8 +1,8 @@
 class Checkout
-    attr_reader :scanned_items, :promotional_rules
+    attr_reader :items, :promotional_rules
 
-    PROMO1 = Proc.new do |scanned_items, item_total|
-      scanned_items.map{|x| x[1]}.select{|x| x == "Lavender heart"}.length > 1
+    PROMO1 = Proc.new do |items, item_total|
+      items.map{|x| x[1]}.select{|x| x == "Lavender heart"}.length > 1
     end
     PROMO2 = Proc.new do |item_total|
       item_total > 60
@@ -10,25 +10,25 @@ class Checkout
     PROMOTIONAL_RULES = {"promo1":  PROMO1, "promo2": PROMO2}
   
     def initialize(promotional_rules)
-      @scanned_items = []
+      @items = []
       @promotional_rules = promotional_rules
     end
   
     def scan(item)
-      @scanned_items << [item.code, item.name, item.price]
+      @items.push([item.code, item.name, item.price])
     end
   
     def total
       price_index = 2
-      item_total = @scanned_items.map{|item|item.select.with_index{|_,i| i == price_index}}.flatten.reduce(:+)
-      apply_discounts(item_total, @scanned_items)
+      item_total = @items.map{|item|item.select.with_index{|_,i| i == price_index}}.flatten.sum
+      discounts(item_total, @items)
     end
   
-    def discounts(item_total, scanned_items)
+    def discounts(item_total, items)
       old_lavender_price = 9.25
       new_lavender_price = 8.50
-      if @promotional_rules[:promo1].call(scanned_items, item_total)
-        item_total -= ((old_lavender_price - new_lavender_price) * scanned_items.map{|x| x[1]}.select{|x| x == "Lavender heart"}.length)
+      if @promotional_rules[:promo1].call(items, item_total)
+        item_total -= ((old_lavender_price - new_lavender_price) * items.map{|x| x[1]}.select{|x| x == "Lavender heart"}.length)
       end
       if @promotional_rules[:promo2].call(item_total)
         item_total -= (item_total * 0.1)
@@ -36,7 +36,7 @@ class Checkout
       "£#{sprintf("%#.2f",item_total)}"
       
     end
-
+end
 
 
     
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,112 +2,70 @@ require "checkout"
 
 describe Checkout do
 
-    PROMO1 = Proc.new do |scanned_items, item_total|
-        scanned_items.map{|x| x[1]}.select{|x| x == "Lavender heart"}.length > 1
+    PROMO1 = Proc.new do |items, item_total|
+        items.map{|x| x[1]}.select{|x| x == "Lavender heart"}.length > 1
       end
-      PROMO2 = Proc.new do |item_total|
-        item_total > 60
-      end
-      PROMOTIONAL_RULES = {"promo1":  PROMO1, "promo2": PROMO2}
-      subject{ Checkout.new(PROMOTIONAL_RULES) }
+    PROMO2 = Proc.new do |item_total|
+    item_total > 60
+    end
+    PROMOTIONAL_RULES = {"promo1":  PROMO1, "promo2": PROMO2}
+    subject{ Checkout.new(PROMOTIONAL_RULES) }
+
+    before :each do
+    @item1 = double :item, code:"001", name:"Lavender heart", price: 9.25
+    @item2 = double :item, code:"002", name:"Personalised cufflinks", price: 45.00
+    @item3 = double :item, code:"003", name:"Kids T-shirt", price: 19.95
+    end
+
+    it "has an empty list of scanned items when initialized" do
+        expect(subject.items).to eq([])
+    end
     
-      before :each do
-        @item1 = double :item, code:"001", name:"Lavender heart", price: 9.25
-        @item2 = double :item, code:"002", name:"Personalised cufflinks", price: 45.00
-        @item3 = double :item, code:"003", name:"Kids T-shirt", price: 19.95
-      end
+    context "when initialised" do
+        it "has promotional rules" do
+            expect(subject.promotional_rules).to eq ({"promo1":  PROMO1, "promo2": PROMO2})
+        end
+    end
     
-      it "has an empty list of scanned items when initialized" do
-        expect(subject.scanned_items).to eq([])
-      end
-    
-      it "has promotional rules when initialised" do
-        expect(subject.promotional_rules).to eq ({"promo1":  PROMO1, "promo2": PROMO2})
-      end
-    
-      it "can scan an item passed in from a basket and stores in scanned items" do
+  
+    it "can scan an item passed in from a basket and stores in scanned items" do
         subject.scan(@item1)
-        expect(subject.scanned_items).to eq [["001", "Lavender heart", 9.25]]
-      end
-    
-      it "can scan two items passed in from a basket and stores in scanned items" do
-        subject.scan(@item1)
-        subject.scan(@item2)
-        expect(subject.scanned_items).to eq [["001", "Lavender heart", 9.25],["002", "Personalised cufflinks", 45.00]]
-      end
-    
-      it "can calculate the total cost of one item in a basket" do
-        subject.scan(@item1)
-        expect(subject.total).to eq "£9.25"
-      end
-    
-      it "can calculate the total cost of two items in a basket" do
+        expect(subject.items).to eq [["001", "Lavender heart", 9.25]]
+    end
+        
+    it "can scan two items passed in from a basket and stores in scanned items" do
         subject.scan(@item1)
         subject.scan(@item2)
-        expect(subject.total).to eq "£54.25"
-      end
+        expect(subject.items).to eq [["001", "Lavender heart", 9.25],["002", "Personalised cufflinks", 45.00]]
+    end
+        
+        it "can calculate the total cost of one item in a basket" do
+            subject.scan(@item1)
+            expect(subject.total).to eq "£9.25"
+        end
+        
+        it "can calculate the total cost of two items in a basket" do
+            subject.scan(@item1)
+            subject.scan(@item2)
+            expect(subject.total).to eq "£54.25"
+        end
+
+    context "when spend over £60" do    
+        it "can apply discount of 10% " do
+            subject.scan(@item1)
+            subject.scan(@item2)
+            subject.scan(@item3)
+            expect(subject.total).to eq "£66.78"
+        end
+    end
     
-      it "can apply discount of 10% when spend over £60" do
-        subject.scan(@item1)
-        subject.scan(@item2)
-        subject.scan(@item3)
-        expect(subject.total).to eq "£66.78"
-      end
-    
-      it "can apply discount when buy 2 or more Lavender hearts" do
-        subject.scan(@item1)
-        subject.scan(@item1)
-        subject.scan(@item3)
-        expect(subject.total).to eq "£36.95"
-      end
-    
-     
-    # describe '.new' do
-    #     it "takes one argument" do             
-    #         expect(Checkout).to respond_to(:new).with(1).argument
-    #     end
-    # end
-    
-    # it { is_expected.to respond_to(:scan).with(1).argument }
-
-    # it { is_expected.to respond_to(:total) }
-
-    # it { is_expected.to respond_to(:item)}
-
-    # it 'returns Lavender heart when scanned item 001 is scanned' do
-    #     item = "001"
-    #     expect(subject.scan(item)).to eq({code: "001", name: "Lavender heart", price: 9.25})
-    # end
-
-    # it 'returns Personalised cufflinks when scanned item 002 is scanned' do
-    #     item = "002"
-    #     expect(subject.scan(item)).to eq({:code=>"002", :name=>"Personalised cufflinks", :price=>45.00})
-    # end
-
-    # it 'returns Kids T-shirts when scanned item 002 is scanned' do
-    #     item = "003"
-    #     expect(subject.scan(item)).to eq({:code=>"003", :name=>"Kids T-shirt", :price=>19.95})
-    # end
-
-
-    # it "returns £9.25 when item '001' is scanned" do
-    #     item = "001"
-    #     subject.scan(item)
-    #     expect(subject.total).to eq "£9.25"
-    # end
-
-    # it "returns £39.90 when two '003' items are scanned" do
-    #     item = "003"
-    #     subject.scan(item)
-    #     subject.scan(item)
-    #     expect(subject.total).to eq "£39.90"
-    # end
-
-    # it "returns £81.00 when two '002' Personalised cufflinks are purchased" do
-    #     item = "002"
-    #     subject.scan(item)
-    #     subject.scan(item)
-    #     expect(subject.total).to eq "£81.90"
-    # end
+    context "when buy 2 or more Lavender hearts" do
+        it "can apply discount " do
+            subject.scan(@item1)
+            subject.scan(@item1)
+            subject.scan(@item3)
+            expect(subject.total).to eq "£36.95"
+        end
+    end
 end
 
