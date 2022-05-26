a1b6759cc4be8035cdbfbb84746c2655337ac3be
Mrkaykay <90140075+Mrkaykay@users.noreply.github.com>
2022-05-26 08:59:46 +0100

    Checkout responds to scan

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,5 +1,14 @@
 class Checkout
-    def initialize(promotional_rules)
+    PROMO =  {over60: ".10", two_or_more: "-8."}
+
+    def initialize(promotional_rules = PROMO)
     end
+
+    def scan
+    end
+
+
+
+    
 
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,10 +1,15 @@
 require "checkout"
 
 describe Checkout do
+     
     describe '.new' do
-        it "takes one argument" do
+        it "takes one argument" do             
             expect(Checkout).to respond_to(:new).with(1).argument
         end
     end
+    
+    it { is_expected.to respond_to :scan}
+
+
 end
 
