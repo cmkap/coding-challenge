50fb773116a90a7ccd55f83c5dd7030020015fba
Mrkaykay <90140075+Mrkaykay@users.noreply.github.com>
2022-05-26 16:26:13 +0100

    Refactored code to include items object and added promotional rules

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,28 +1,42 @@
 class Checkout
-    attr_reader :item
+    attr_reader :scanned_items, :promotional_rules
 
-    PROMO =  {over60: ".10", two_or_more: "-8."}
-    PRODUCTS = [
-        {:code=>"001", :name=>"Lavender heart", :price=>9.25},
-        {:code=>"002", :name=>"Personalised cufflinks", :price=>45.00},
-        {:code=>"003", :name=>"Kids T-shirt", :price=>19.95}
-    ]
-
-
-    def initialize(promotional_rules = PROMO)
-        @basket = 0
+    PROMO1 = Proc.new do |scanned_items, item_total|
+      scanned_items.map{|x| x[1]}.select{|x| x == "Lavender heart"}.length > 1
     end
-
+    PROMO2 = Proc.new do |item_total|
+      item_total > 60
+    end
+    PROMOTIONAL_RULES = {"promo1":  PROMO1, "promo2": PROMO2}
+  
+    def initialize(promotional_rules)
+      @scanned_items = []
+      @promotional_rules = promotional_rules
+    end
+  
     def scan(item)
-        product = PRODUCTS.select do |product|
-            product[:code].include? item 
-        end
-
-        @item = product[0]
+      @scanned_items << [item.code, item.name, item.price]
     end
-
+  
     def total
-        "£#{sprintf("%#.2f",@basket)}"
+      price_index = 2
+      item_total = @scanned_items.map{|item|item.select.with_index{|_,i| i == price_index}}.flatten.reduce(:+)
+      apply_discounts(item_total, @scanned_items)
+    end
+  
+    def apply_discounts(item_total, scanned_items)
+      old_lavender_price = 9.25
+      new_lavender_price = 8.50
+      if @promotional_rules[:promo1].call(scanned_items, item_total)
+        item_total -= ((old_lavender_price - new_lavender_price) * scanned_items.map{|x| x[1]}.select{|x| x == "Lavender heart"}.length)
+      end
+      if @promotional_rules[:promo2].call(item_total)
+        item_total -= (item_total * 0.1)
+      end
+      "£#{sprintf("%#.2f",item_total)}"
+      
     end
 
-end
+
+
+    
